ae3905d3aff98fc7fa81b27e07770f5f421ac97b
fnukyy <fnukyxx@gmail.com>
2021-04-24 20:39:48 +0200

    Connected to database
    1.0.0

---
+++ app.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const mongoose = require('./database/mongoose');
+const fs = require('fs');
+require('dotenv').config();
+
+client.prefix = '!';
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
+mongoose.init();
+client.login(process.env.TOKEN);
